43daab0f6ff7d950ce06a12ad701aa4b3388e822
Magnus Kråbøl <magnus.krabol@kantega.no>
2023-11-20 23:25:31 +0100

    wishlist page in progress

--- bryllup/src/componets/pages/Wishlist.jsx
+++ bryllup/src/componets/pages/Wishlist.jsx
@@ -1,9 +1,22 @@
-import Heading from "../layout/Heading"
+import React from "react";
+import wishesData from '../../assets/wishes.json';
+import Heading from "../layout/Heading";
 
-function Wishlist () {
-    return ( 
-        <Heading size="2" content={"Ønskeliste"} />
+function Wishlist() {
+    return (
+        <div>
+            <Heading />
+            <ul>
+                {wishesData.wishes.map((wish, index) => (
+                    <li key={index}>
+                        <h3>{wish.wishName}</h3>
+                        
+                        <a href={wish.url}>View Wish</a>
+                    </li>
+                ))}
+            </ul>
+        </div>
     )
 }
 
-export default Wishlist
+export default Wishlist;
