d29e8db8bf0d0c0f471636e2b387be59fae0d175
Holmfred <fredrik1304@live.no>
2023-11-24 18:50:48 +0100

    Made Nav Responsive

--- bryllup/src/componets/layout/Nav.jsx
+++ bryllup/src/componets/layout/Nav.jsx
@@ -1,48 +1,56 @@
-import Nav from 'react-bootstrap/Nav';
 import { NavLink } from "react-router-dom";
-import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 import Logo from './../../assets/Logo.png';
 
 
+
 export default function NAV() {
-  
+
   return (
-    <Container>
+    <header id="header">
       <NavLink to="/" className="brand-logo-mobile">
-        <img src={Logo} alt="Logo" className='brand-logo-image'/>
+        <img src={Logo} alt="Logo" className="brand-logo-image" />
       </NavLink>
-      <NavLink to="/" className="brand-logo-desktop">
-        <img src={Logo} alt="Logo" className='brand-logo-image'/>
-      </NavLink>
-    <Navbar collapseOnSelect expand="lg" className="navbar">
-      <Container className='nav-container'>
-        <Navbar.Collapse id="resposive-navbar-nav">
-          <Nav className='mr-auto'>
-            <div className='navbar-link-left'>
-              <NavLink to="/" className="nav-link"> 
-                    Forside
-              </NavLink>
-              <NavLink to="/Program" className="nav-link">
-                    Program
-              </NavLink>
-              <NavLink to="/Guests" className="nav-link">
-                    Gjesteliste
-              </NavLink>
-            </div>
-            <div className='navbar-link-right'>
-              <NavLink eventkey="4" as={NavLink} to="/Info" className="nav-link">
-                    Generell Info
-              </NavLink>
-              <NavLink to="/Wishlist" className="nav-link">
-                    Ønskeliste
-              </NavLink>
-            </div>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar> 
-  </Container>
-  
+      <Navbar expand="lg">
+        <NavLink to="/" className="brand-logo-desktop">
+          <img src={Logo} alt="Logo" className="brand-logo-image" />
+        </NavLink>
+        <Container className="nav-container">
+
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" className="hamburger">
+            <i className="fa-solid fa-bars" ></i>
+          </Navbar.Toggle>
+
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="nav-container">
+              <div className="nav-container-left"> 
+                <NavLink to="/" className="nav-link">
+                  Forside
+                </NavLink>
+                <NavLink to="/Program" className="nav-link">
+                  Program
+                </NavLink>
+                <NavLink to="/Guests" className="nav-link">
+                  Gjesteliste
+                </NavLink>
+              </div>
+              <div className="nav-container-right">
+                <NavLink to="/Info" className="nav-link">
+                  Generell Info
+                </NavLink>
+                <NavLink to="/Wishlist" className="nav-link">
+                  Ønskeliste
+                </NavLink>
+                <NavLink to="/" className="nav-link hidden">
+                  Hidden
+                </NavLink>
+              </div>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
   );
 }
