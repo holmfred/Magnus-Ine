e0e0c271df0914ba6918b2b7224139e6fdd508ae
Magnus Kråbøl <magnus.krabol@kantega.no>
2023-11-26 13:28:54 +0100

    text on guest page

--- bryllup/src/componets/pages/Guests.jsx
+++ bryllup/src/componets/pages/Guests.jsx
@@ -2,7 +2,11 @@ import Heading from "../layout/Heading"
 
 function Guests () {
     return ( 
+        <>
         <Heading size="2" content={"Gjesteliste"} />
+        <p>Gjestelisten oppdateres etterhvert som vi får tilbakemelding på hvem som kan komme. </p>
+        <p>Når fristen for påmelding er gått ut 1. april, blir bordsettingen illustrert her.</p>
+        </>
     )
 }
 
